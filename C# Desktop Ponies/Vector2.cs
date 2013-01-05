2f9593a3ff5ac1c4771e97055860be9a96b4aaf5
RoosterDragon <TThomaso@hotmail.co.uk>
2013-01-05 23:17:45 +0000

    Doc'd Vector2 and Vector2F and added Distance, DistanceSquared and Length methods to these classes.

--- C# Desktop Ponies/Vector2.cs
+++ C# Desktop Ponies/Vector2.cs
@@ -52,46 +52,110 @@
             Y = size.Height;
         }
 
+        /// <summary>
+        /// Returns the negation of this vector.
+        /// </summary>
+        /// <param name="v">The vector to negate.</param>
+        /// <returns>A new vector whose components have both been negated.</returns>
         public static Vector2 operator -(Vector2 v)
         {
             return new Vector2(-v.X, -v.Y);
         }
+        /// <summary>
+        /// Performs vector subtraction.
+        /// </summary>
+        /// <param name="left">The vector to the left of the subtraction operator.</param>
+        /// <param name="right">The vector to the right of the subtraction operator.</param>
+        /// <returns>A new vector where both components are calculated by subtracting the values in the second vector from the first.</returns>
         public static Vector2 operator -(Vector2 left, Vector2 right)
         {
             return new Vector2(left.X - right.X, left.Y - right.Y);
         }
+        /// <summary>
+        /// Performs vector addition.
+        /// </summary>
+        /// <param name="left">The vector to the left of the addition operator.</param>
+        /// <param name="right">The vector to the right of the addition operator.</param>
+        /// <returns>A new vector where both components are calculated by adding the components from the source vectors.</returns>
         public static Vector2 operator +(Vector2 left, Vector2 right)
         {
             return new Vector2(left.X + right.X, left.Y + right.Y);
         }
+        /// <summary>
+        /// Multiplies a vector by a scalar.
+        /// </summary>
+        /// <param name="v">The vector to multiply.</param>
+        /// <param name="scalar">The scale factor to apply.</param>
+        /// <returns>A new vector where both components are calculated by multiplying their value by the scale factor.</returns>
         public static Vector2 operator *(Vector2 v, int scalar)
         {
             return new Vector2(v.X * scalar, v.Y * scalar);
         }
+        /// <summary>
+        /// Divides a vector by a scalar, using integer division.
+        /// </summary>
+        /// <param name="v">The vector to divide.</param>
+        /// <param name="scalar">The scale factor to apply.</param>
+        /// <returns>A new vector where both components are calculated by dividing their value by the scale factor, using integer division.
+        /// </returns>
         public static Vector2 operator /(Vector2 v, int scalar)
         {
             return new Vector2(v.X / scalar, v.Y / scalar);
         }
+        /// <summary>
+        /// Multiplies a vector by a scalar.
+        /// </summary>
+        /// <param name="v">The vector to multiply.</param>
+        /// <param name="scalar">The scale factor to apply.</param>
+        /// <returns>A new vector where both components are calculated by multiplying their value by the scale factor.</returns>
         public static Vector2F operator *(Vector2 v, float scalar)
         {
             return new Vector2F(v.X * scalar, v.Y * scalar);
         }
+        /// <summary>
+        /// Divides a vector by a scalar.
+        /// </summary>
+        /// <param name="v">The vector to divide.</param>
+        /// <param name="scalar">The scale factor to apply.</param>
+        /// <returns>A new vector where both components are calculated by dividing their value by the scale factor.
+        /// </returns>
         public static Vector2F operator /(Vector2 v, float scalar)
         {
             return new Vector2F(v.X / scalar, v.Y / scalar);
         }
+        /// <summary>
+        /// Determines whether two vectors are equal.
+        /// </summary>
+        /// <param name="left">The vector to the left of the equality operator.</param>
+        /// <param name="right">The vector to the right of the equality operator.</param>
+        /// <returns>Returns true if each component pair is considered equal; otherwise, false.</returns>
         public static bool operator ==(Vector2 left, Vector2 right)
         {
             return left.X == right.X && left.Y == right.Y;
         }
+        /// <summary>
+        /// Determines whether two vectors are unequal.
+        /// </summary>
+        /// <param name="left">The vector to the left of the inequality operator.</param>
+        /// <param name="right">The vector to the right of the inequality operator.</param>
+        /// <returns>Returns true any component pair is considered unequal; otherwise, false.</returns>
         public static bool operator !=(Vector2 left, Vector2 right)
         {
             return !(left == right);
         }
+        /// <summary>
+        /// Returns a representation of this vector in Cartesian notation, e.g. "(0, 0)".
+        /// </summary>
+        /// <returns>A string representation of this vector.</returns>
         public override string ToString()
         {
             return string.Format(System.Globalization.CultureInfo.CurrentCulture, "({0}, {1})", X, Y);
         }
+        /// <summary>
+        /// Tests whether the specified object is a vector structure and is equal to this vector.
+        /// </summary>
+        /// <param name="obj">The object to test.</param>
+        /// <returns>Returns true is <paramref name="obj"/> is a vector structure equivalent to this vector; otherwise, false.</returns>
         public override bool Equals(object obj)
         {
             if (obj == null || !(obj is Vector2))
@@ -99,40 +163,103 @@
 
             return this == (Vector2)obj;
         }
+        /// <summary>
+        /// Returns a hash code for this vector structure.
+        /// </summary>
+        /// <returns>An integer value that specifies the hash code for this vector.</returns>
         public override int GetHashCode()
         {
             return X ^ Y;
         }
 
+        /// <summary>
+        /// Implicitly converts a point into a vector structure.
+        /// </summary>
+        /// <param name="p">The point to convert.</param>
+        /// <returns>A new point whose x- and y- coordinates are used to initialize the vector.</returns>
         public static implicit operator Vector2(Point p)
         {
-            return new Vector2(p.X, p.Y);
+            return new Vector2(p);
         }
 
+        /// <summary>
+        /// Implicitly converts a vector with integer components into a vector with floating point components.
+        /// </summary>
+        /// <param name="v">The vector to convert.</param>
+        /// <returns>A new vector initialized with the same component values as the specified vector.</returns>
         public static implicit operator Vector2F(Vector2 v)
         {
             return new Vector2F(v.X, v.Y);
         }
+        /// <summary>
+        /// Implicitly converts a vector into a point structure.
+        /// </summary>
+        /// <param name="v">The vector to convert.</param>
+        /// <returns>A new point whose horizontal and vertical components are initialized from the specified vector.</returns>
         public static implicit operator Point(Vector2 v)
         {
             return new Point(v.X, v.Y);
         }
+        /// <summary>
+        /// Implicitly converts a vector into a size structure.
+        /// </summary>
+        /// <param name="v">The vector to convert.</param>
+        /// <returns>A new size whose width and height components are initialized from the specified vector.</returns>
         public static implicit operator Size(Vector2 v)
         {
             return new Size(v.X, v.Y);
         }
 
+        /// <summary>
+        /// Determines whether two vectors are equal.
+        /// </summary>
+        /// <param name="left">The vector to the left of the equality operator.</param>
+        /// <param name="right">The vector to the right of the equality operator.</param>
+        /// <returns>Returns true if each component pair is considered equal; otherwise, false.</returns>
         public static bool Equals(Vector2 left, Vector2 right)
         {
             return left == right;
         }
+        /// <summary>
+        /// Determines whether the specified vector is equal to this vector.
+        /// </summary>
+        /// <param name="other">The vector to test.</param>
+        /// <returns>Returns true if each component pair is considered equal; otherwise, false.</returns>
         public bool Equals(Vector2 other)
         {
             return this == other;
         }
+
+        /// <summary>
+        /// Calculates the distance between two vectors.
+        /// </summary>
+        /// <param name="a">First vector.</param>
+        /// <param name="b">Second vector.</param>
+        /// <returns>The distance between the two vectors.</returns>
+        public static float Distance(Vector2 a, Vector2 b)
+        {
+            return (float)Math.Sqrt(DistanceSquared(a, b));
+        }
+        /// <summary>
+        /// Calculates the square of the distance between two vectors.
+        /// </summary>
+        /// <param name="a">First vector.</param>
+        /// <param name="b">Second vector.</param>
+        /// <returns>The square of the distance between the two vectors.</returns>
+        public static int DistanceSquared(Vector2 a, Vector2 b)
+        {
+            return (a.X - b.X) * (a.X - b.X) + (a.Y - b.Y) * (a.Y - b.Y);
+        }
+        /// <summary>
+        /// Calculates the length of the vector.
+        /// </summary>
+        /// <returns>The distance of this vector from the origin.</returns>
+        public float Length()
+        {
+            return Distance(this, Zero);
+        }
     }
     
-    //     
     /// <summary>
     /// Represents an ordered pair of floating-point x- and y-coordinates that defines a point in a two-dimensional plane.
     /// </summary>
@@ -182,38 +309,89 @@
             Y = size.Height;
         }
 
+        /// <summary>
+        /// Returns the negation of this vector.
+        /// </summary>
+        /// <param name="v">The vector to negate.</param>
+        /// <returns>A new vector whose components have both been negated.</returns>
         public static Vector2F operator -(Vector2F v)
         {
             return new Vector2F(-v.X, -v.Y);
         }
+        /// <summary>
+        /// Performs vector subtraction.
+        /// </summary>
+        /// <param name="left">The vector to the left of the subtraction operator.</param>
+        /// <param name="right">The vector to the right of the subtraction operator.</param>
+        /// <returns>A new vector where both components are calculated by subtracting the values in the second vector from the first.</returns>
         public static Vector2F operator -(Vector2F left, Vector2F right)
         {
             return new Vector2F(left.X - right.X, left.Y - right.Y);
         }
+        /// <summary>
+        /// Performs vector addition.
+        /// </summary>
+        /// <param name="left">The vector to the left of the addition operator.</param>
+        /// <param name="right">The vector to the right of the addition operator.</param>
+        /// <returns>A new vector where both components are calculated by adding the components from the source vectors.</returns>
         public static Vector2F operator +(Vector2F left, Vector2F right)
         {
             return new Vector2F(left.X + right.X, left.Y + right.Y);
         }
+        /// <summary>
+        /// Multiplies a vector by a scalar.
+        /// </summary>
+        /// <param name="v">The vector to multiply.</param>
+        /// <param name="scalar">The scale factor to apply.</param>
+        /// <returns>A new vector where both components are calculated by multiplying their value by the scale factor.</returns>
         public static Vector2F operator *(Vector2F v, float scalar)
         {
             return new Vector2F(v.X * scalar, v.Y * scalar);
         }
+        /// <summary>
+        /// Divides a vector by a scalar.
+        /// </summary>
+        /// <param name="v">The vector to divide.</param>
+        /// <param name="scalar">The scale factor to apply.</param>
+        /// <returns>A new vector where both components are calculated by dividing their value by the scale factor.
+        /// </returns>
         public static Vector2F operator /(Vector2F v, float scalar)
         {
             return new Vector2F(v.X / scalar, v.Y / scalar);
         }
+        /// <summary>
+        /// Determines whether two vectors are equal.
+        /// </summary>
+        /// <param name="left">The vector to the left of the equality operator.</param>
+        /// <param name="right">The vector to the right of the equality operator.</param>
+        /// <returns>Returns true if each component pair is considered equal; otherwise, false.</returns>
         public static bool operator ==(Vector2F left, Vector2F right)
         {
             return left.X == right.X && left.Y == right.Y;
         }
+        /// <summary>
+        /// Determines whether two vectors are unequal.
+        /// </summary>
+        /// <param name="left">The vector to the left of the inequality operator.</param>
+        /// <param name="right">The vector to the right of the inequality operator.</param>
+        /// <returns>Returns true any component pair is considered unequal; otherwise, false.</returns>
         public static bool operator !=(Vector2F left, Vector2F right)
         {
             return !(left == right);
         }
+        /// <summary>
+        /// Returns a representation of this vector in Cartesian notation, e.g. "(0, 0)".
+        /// </summary>
+        /// <returns>A string representation of this vector.</returns>
         public override string ToString()
         {
             return string.Format(System.Globalization.CultureInfo.CurrentCulture, "({0}, {1})", X, Y);
         }
+        /// <summary>
+        /// Tests whether the specified object is a vector structure and is equal to this vector.
+        /// </summary>
+        /// <param name="obj">The object to test.</param>
+        /// <returns>Returns true is <paramref name="obj"/> is a vector structure equivalent to this vector; otherwise, false.</returns>
         public override bool Equals(object obj)
         {
             if (obj == null || !(obj is Vector2F))
@@ -221,27 +399,81 @@
 
             return this == (Vector2F)obj;
         }
+        /// <summary>
+        /// Returns a hash code for this vector structure.
+        /// </summary>
+        /// <returns>An integer value that specifies the hash code for this vector.</returns>
         public override int GetHashCode()
         {
             return X.GetHashCode() ^ Y.GetHashCode();
         }
 
+        /// <summary>
+        /// Implicitly converts a vector into a point structure.
+        /// </summary>
+        /// <param name="v">The vector to convert.</param>
+        /// <returns>A new point whose horizontal and vertical components are initialized from the specified vector.</returns>
         public static implicit operator PointF(Vector2F v)
         {
             return new PointF(v.X, v.Y);
         }
+        /// <summary>
+        /// Implicitly converts a vector into a size structure.
+        /// </summary>
+        /// <param name="v">The vector to convert.</param>
+        /// <returns>A new size whose width and height components are initialized from the specified vector.</returns>
         public static implicit operator SizeF(Vector2F v)
         {
             return new SizeF(v.X, v.Y);
         }
 
+        /// <summary>
+        /// Determines whether two vectors are equal.
+        /// </summary>
+        /// <param name="left">The vector to the left of the equality operator.</param>
+        /// <param name="right">The vector to the right of the equality operator.</param>
+        /// <returns>Returns true if each component pair is considered equal; otherwise, false.</returns>
         public static bool Equals(Vector2F left, Vector2F right)
         {
             return left == right;
         }
+        /// <summary>
+        /// Determines whether the specified vector is equal to this vector.
+        /// </summary>
+        /// <param name="other">The vector to test.</param>
+        /// <returns>Returns true if each component pair is considered equal; otherwise, false.</returns>
         public bool Equals(Vector2F other)
         {
             return this == other;
+        }
+
+        /// <summary>
+        /// Calculates the distance between two vectors.
+        /// </summary>
+        /// <param name="a">First vector.</param>
+        /// <param name="b">Second vector.</param>
+        /// <returns>The distance between the two vectors.</returns>
+        public static float Distance(Vector2F a, Vector2F b)
+        {
+            return (float)Math.Sqrt(DistanceSquared(a, b));
+        }
+        /// <summary>
+        /// Calculates the square of the distance between two vectors.
+        /// </summary>
+        /// <param name="a">First vector.</param>
+        /// <param name="b">Second vector.</param>
+        /// <returns>The square of the distance between the two vectors.</returns>
+        public static float DistanceSquared(Vector2F a, Vector2F b)
+        {
+            return (a.X - b.X) * (a.X - b.X) + (a.Y - b.Y) * (a.Y - b.Y);
+        }
+        /// <summary>
+        /// Calculates the length of the vector.
+        /// </summary>
+        /// <returns>The distance of this vector from the origin.</returns>
+        public float Length()
+        {
+            return Distance(this, Zero);
         }
     }
 }
