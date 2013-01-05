6c1eddf4fc4d6d01bfbb62b5368a170d30453dbc
RoosterDragon <TThomaso@hotmail.co.uk>
2013-01-05 18:47:51 +0000

    Updated versions.

--- VB Desktop Ponies/My Project/AssemblyInfo.vb
+++ VB Desktop Ponies/My Project/AssemblyInfo.vb
@@ -35,6 +35,6 @@ Imports System.Runtime.InteropServices
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
 <Assembly: AssemblyVersion("1.42.*")> 
-<Assembly: AssemblyFileVersion("1.42.0.5")> 
+<Assembly: AssemblyFileVersion("1.42.0.6")> 
 
 <Assembly: NeutralResourcesLanguageAttribute("en")> 
