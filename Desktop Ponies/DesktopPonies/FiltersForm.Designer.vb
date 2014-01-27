c0123d48fa92258c4c3b5f00c89adbec8917a939
RoosterDragon <RoosterDragon@outlook.com>
2014-01-27 16:59:18 +0000

    Tidied up FiltersForm so it would resize nicely.

--- Desktop Ponies/DesktopPonies/FiltersForm.Designer.vb
+++ Desktop Ponies/DesktopPonies/FiltersForm.Designer.vb
@@ -22,80 +22,118 @@ Partial Class FiltersForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Filters_Box = New System.Windows.Forms.TextBox()
-        Me.Label1 = New System.Windows.Forms.Label()
+        Me.FiltersTextBox = New System.Windows.Forms.TextBox()
+        Me.InfoLabel = New System.Windows.Forms.Label()
         Me.Save_Button = New System.Windows.Forms.Button()
         Me.Cancel_Button = New System.Windows.Forms.Button()
-        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SaveLabel = New System.Windows.Forms.Label()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.TableLayoutPanel1.SuspendLayout()
         Me.SuspendLayout()
         '
-        'Filters_Box
+        'FiltersTextBox
         '
-        Me.Filters_Box.Location = New System.Drawing.Point(39, 69)
-        Me.Filters_Box.Multiline = True
-        Me.Filters_Box.Name = "Filters_Box"
-        Me.Filters_Box.ScrollBars = System.Windows.Forms.ScrollBars.Both
-        Me.Filters_Box.Size = New System.Drawing.Size(399, 235)
-        Me.Filters_Box.TabIndex = 0
+        Me.TableLayoutPanel1.SetColumnSpan(Me.FiltersTextBox, 2)
+        Me.FiltersTextBox.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.FiltersTextBox.Location = New System.Drawing.Point(3, 22)
+        Me.FiltersTextBox.Multiline = True
+        Me.FiltersTextBox.Name = "FiltersTextBox"
+        Me.FiltersTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both
+        Me.FiltersTextBox.Size = New System.Drawing.Size(378, 76)
+        Me.FiltersTextBox.TabIndex = 1
         '
-        'Label1
+        'InfoLabel
         '
-        Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(78, 39)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(314, 13)
-        Me.Label1.TabIndex = 1
-        Me.Label1.Text = "Edit the possible pony categories/tags/filters below (one per line)."
+        Me.InfoLabel.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.InfoLabel.AutoSize = True
+        Me.TableLayoutPanel1.SetColumnSpan(Me.InfoLabel, 2)
+        Me.InfoLabel.Location = New System.Drawing.Point(35, 3)
+        Me.InfoLabel.Margin = New System.Windows.Forms.Padding(3)
+        Me.InfoLabel.Name = "InfoLabel"
+        Me.InfoLabel.Size = New System.Drawing.Size(314, 13)
+        Me.InfoLabel.TabIndex = 0
+        Me.InfoLabel.Text = "Edit the possible pony categories/tags/filters below (one per line)."
+        Me.InfoLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'Save_Button
         '
-        Me.Save_Button.Location = New System.Drawing.Point(311, 355)
+        Me.Save_Button.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Save_Button.Location = New System.Drawing.Point(58, 136)
         Me.Save_Button.Name = "Save_Button"
         Me.Save_Button.Size = New System.Drawing.Size(75, 23)
-        Me.Save_Button.TabIndex = 2
-        Me.Save_Button.Text = "Change"
+        Me.Save_Button.TabIndex = 3
+        Me.Save_Button.Text = "Apply"
         Me.Save_Button.UseVisualStyleBackColor = True
         '
         'Cancel_Button
         '
-        Me.Cancel_Button.Location = New System.Drawing.Point(81, 355)
+        Me.Cancel_Button.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.Cancel_Button.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.Cancel_Button.Location = New System.Drawing.Point(250, 136)
         Me.Cancel_Button.Name = "Cancel_Button"
         Me.Cancel_Button.Size = New System.Drawing.Size(75, 23)
-        Me.Cancel_Button.TabIndex = 3
-        Me.Cancel_Button.Text = "CANCEL"
+        Me.Cancel_Button.TabIndex = 4
+        Me.Cancel_Button.Text = "Cancel"
         Me.Cancel_Button.UseVisualStyleBackColor = True
         '
-        'Label2
+        'SaveLabel
         '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(3, 325)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(468, 13)
-        Me.Label2.TabIndex = 4
-        Me.Label2.Text = "Note that you still need to save your settings on the options menu to make the ch" & _
+        Me.SaveLabel.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.SaveLabel.AutoSize = True
+        Me.TableLayoutPanel1.SetColumnSpan(Me.SaveLabel, 2)
+        Me.SaveLabel.Location = New System.Drawing.Point(5, 104)
+        Me.SaveLabel.Margin = New System.Windows.Forms.Padding(3)
+        Me.SaveLabel.Name = "SaveLabel"
+        Me.SaveLabel.Size = New System.Drawing.Size(373, 26)
+        Me.SaveLabel.TabIndex = 2
+        Me.SaveLabel.Text = "Note that you still need to save your settings on the options menu to make the ch" & _
     "ange permanent!"
+        Me.SaveLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 2
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.Controls.Add(Me.InfoLabel, 0, 0)
+        Me.TableLayoutPanel1.Controls.Add(Me.Cancel_Button, 1, 3)
+        Me.TableLayoutPanel1.Controls.Add(Me.SaveLabel, 0, 2)
+        Me.TableLayoutPanel1.Controls.Add(Me.Save_Button, 0, 3)
+        Me.TableLayoutPanel1.Controls.Add(Me.FiltersTextBox, 0, 1)
+        Me.TableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 4
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle())
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle())
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle())
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(384, 162)
+        Me.TableLayoutPanel1.TabIndex = 0
         '
         'FiltersForm
         '
+        Me.AcceptButton = Me.Save_Button
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.AppWorkspace
-        Me.ClientSize = New System.Drawing.Size(469, 400)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.Cancel_Button)
-        Me.Controls.Add(Me.Save_Button)
-        Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.Filters_Box)
+        Me.CancelButton = Me.Cancel_Button
+        Me.ClientSize = New System.Drawing.Size(384, 162)
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.MinimumSize = New System.Drawing.Size(300, 175)
         Me.Name = "FiltersForm"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
         Me.Text = "Edit Filters/Tags..."
+        Me.TableLayoutPanel1.ResumeLayout(False)
+        Me.TableLayoutPanel1.PerformLayout()
         Me.ResumeLayout(False)
-        Me.PerformLayout()
 
     End Sub
-    Friend WithEvents Filters_Box As System.Windows.Forms.TextBox
-    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents FiltersTextBox As System.Windows.Forms.TextBox
+    Friend WithEvents InfoLabel As System.Windows.Forms.Label
     Friend WithEvents Save_Button As System.Windows.Forms.Button
     Friend WithEvents Cancel_Button As System.Windows.Forms.Button
-    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents SaveLabel As System.Windows.Forms.Label
+    Friend WithEvents TableLayoutPanel1 As System.Windows.Forms.TableLayoutPanel
 End Class
--- Desktop Ponies/DesktopPonies/FiltersForm.vb
+++ Desktop Ponies/DesktopPonies/FiltersForm.vb
@@ -4,8 +4,8 @@
         Icon = My.Resources.Twilight
     End Sub
 
-    Private Sub Filters_Form_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Filters_Box.Lines = Options.CustomTags.Select(Function(tag) tag.ToString()).ToArray()
+    Private Sub FiltersForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        FiltersTextBox.Lines = Options.CustomTags.Select(Function(tag) tag.ToString()).ToArray()
     End Sub
 
     Private Sub Cancel_Button_Click(sender As Object, e As EventArgs) Handles Cancel_Button.Click
@@ -13,7 +13,7 @@
     End Sub
 
     Private Sub Save_Button_Click(sender As Object, e As EventArgs) Handles Save_Button.Click
-        Dim lines = Filters_Box.Lines.ToList()
+        Dim lines = FiltersTextBox.Lines.ToList()
         If lines.RemoveAll(Function(line) line.IndexOf(ControlChars.Quote) <> -1) > 0 Then
             MessageBox.Show(Me, "You cannot use the quote character in custom tags. Any tags with this character have been removed.",
                             "Invalid Tags", MessageBoxButtons.OK, MessageBoxIcon.Warning)
